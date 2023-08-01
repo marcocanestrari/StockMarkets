35b7e532db1788e9d966b5911aef64bdf4eae5ea
Marco Canestrari <marco.canestrari@gmail.com>
2023-08-01 21:37:21 +0100

    Added supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "StockMarkets",
+    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
